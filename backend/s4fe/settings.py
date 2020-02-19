e8d3cde541f509efe8853bffe77057fa6a80781e
goran1985 <goran@s4fe.io>
2020-02-19 09:04:32 +0100

    Deploy Attempt 2

--- backend/s4fe/settings.py
+++ backend/s4fe/settings.py
@@ -22,9 +22,6 @@ DEBUG = env.bool("DEBUG", default=False)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY", "")
 
