a0f56a6fa8e9fda0fd016b824d5c8e1d8d9a0179
goran1985 <goran@s4fe.io>
2020-02-20 11:45:18 +0100

    Models added into admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Item, Category, Message, OTP
 
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'key', 'user', 'category', 'status')
+    list_filter = ['user', 'status']
+    search_fields = ('title', 'key', 'user', 'category', 'status')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description',)
+    search_fields = ('title', 'description',)
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'content', 'is_read')
+    list_filter = ['sender', 'receiver', 'is_read']
+    search_fields = ('content', )
+
+
+class OTPAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'otp')
+    search_fields = ('phone_number', 'otp')
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(OTP, OTPAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from users.models import User
+from django.contrib.auth import admin as auth_admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 
-admin.site.register(User)
+from users.forms import UserChangeForm, UserCreationForm
+
+User = get_user_model()
+
+admin.site.unregister(Group)
+
+
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+
+    form = UserChangeForm
+    add_form = UserCreationForm
+    fieldsets = (("User", {"fields": ("name",)}),) + auth_admin.UserAdmin.fieldsets
+    list_display = ["username", "email", "first_name", "last_name", "phone_number", "is_superuser",]
+    search_fields = ["email"]
