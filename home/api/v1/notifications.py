b1648e4ba7532340c4564f0598551aae28e7042f
markan1985 <goran@crowdbotics.com>
2021-05-13 13:51:39 +0200

    fx

--- home/api/v1/notifications.py
+++ home/api/v1/notifications.py
@@ -4,7 +4,7 @@ from fcm_django.models import FCMDevice
 def push_notification(title, user, sender, message):
     try:
         devices = FCMDevice.objects.filter(user=user)
-        devices.send_message(title=title, body=message, data={"user_id", "1"})
+        devices.send_message(title=title, body=message, badge=sender.id)
         return True
 
     except Exception:
