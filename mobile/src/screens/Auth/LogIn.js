dde226553971d4000d396334ada4616f257fafe8
Shone Radulovic <neven@@s4fe.io>
2021-05-14 10:41:56 +0200

    Fix

--- mobile/src/screens/Auth/LogIn.js
+++ mobile/src/screens/Auth/LogIn.js
@@ -38,7 +38,7 @@ const { height, width } = Dimensions.get('window');
 export default class SignIn extends ValidationComponent {
 	constructor(props) {
 		super(props)
-		this.state = {git log -- android/java_pid14920.hprof
+		this.state = {
 			email: '',
 			password: '',
 			dataLoading: false,
