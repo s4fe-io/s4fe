31559678462dba3ab795aee8cf2fcb447adf403f
nevens4fe <61021399+nevens4fe@users.noreply.github.com>
2022-03-24 09:37:40 +0200

    Update settings.py

--- s4fe/settings.py
+++ s4fe/settings.py
@@ -131,7 +131,6 @@ if env.str("DATABASE_URL", default=None):
         'default1': env.db()
     }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -150,7 +149,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -159,7 +157,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
